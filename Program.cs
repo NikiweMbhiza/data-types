13c52c1b7bd7bb07bbd1462e2f15dda753f59ea3
Nikiw <Nikiw@NIKIWE_MBHIZA>
2024-01-23 09:02:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Nikiwe";
+            int age = 35;
+            bool isTall = false;
+            double weight = 80.4;
+
+            Console.WriteLine(name +" "+ "is" +age+"she weighs"+" "+weight);
+            Console.WriteLine("it is " + isTall + " that she is tall ");
+            Console.ReadKey();
+
+
+        }
+    }
+}
